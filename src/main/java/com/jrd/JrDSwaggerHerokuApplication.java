3fedf6ec94b493bda8e0fdd06ae4a672d87e170d
jr.domingos <domingoshot@hotmail.com>
2020-05-30 23:26:58 -0300

    init

---
+++ src/main/java/com/jrd/JrDSwaggerHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.jrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrDSwaggerHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrDSwaggerHerokuApplication.class, args);
+	}
+
+}
